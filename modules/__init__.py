745a44e118d767aa277e395e8b48462c44bba9cd
Haloroute <topaz.and.numpy@gmail.com>
2026-05-19 08:14:39 +0700

    feat: add NeuCodec as the codec module

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,6 @@
 # Init file
 from .amplitude import LocalRMSAmplitude
+from .codec import DistillNeuCodec, NeuCodec
 from .content import Conv2dSubsampling, EncoderModel, ScheduledFloat, Zipformer2
 from .embedding import LogEmbedding
 from .pitch import FCPE
